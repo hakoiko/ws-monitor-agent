76c549c37d5813a0acab515b5cc73012dc53ab1a
hakoiko <hakoiko@gmail.com>
2017-04-17 17:54:29 +0900

    시스템 정보 표기에 이런저런 데이터들 추가

--- src/app.js
+++ src/app.js
@@ -7,7 +7,13 @@ const ws = new Websocket(Config.AETHER_URL);
 
 let stat = {
 	os: () => {
-		return Sysinfo.os();
+		return new Promise((resolve, reject) => {
+			Sysinfo.os().then((res) => {
+				resolve(res);
+			}, (err) => {
+				reject(err);
+			});
+		});
 	},
 	cpu: () => {
 		return new Promise((resolve, reject) => {
@@ -17,16 +23,35 @@ let stat = {
 				reject(err);
 			});
 		});
+	},
+	mem: () => {
+		return Sysinfo.mem();
+	},
+	fs: () => {
+		return Sysinfo.fs();
+	},
+	oops: 'OOPS',
+	dynamic: function() {
+		return new Promise((resolve, reject) => {
+			Promise.all([this.cpu(), this.mem(), this.fs()]).then((res) => {
+				resolve({
+					cpu: JSON.stringify(res[0]),
+					mem: JSON.stringify(res[1]),
+					fs: JSON.stringify(res[2])
+				});
+			}, (err) => {
+				reject(err);
+			});
+		});
 	}
 };
 
-console.log('OS HOSTNAME: ', stat.os());
-
 ws.on('open', function open() {
 	setInterval(() => {
-		stat.cpu().then((res) => {
-			//console.log('SEND:', res)
-			ws.send(res);
+		stat.dynamic().then((res) => {
+			console.log(Config.ROW);
+			console.log('SEND:', res);
+			ws.send(JSON.stringify(res));
 		}).catch((err) => {
 			console.log(err);
 		});
--- src/sysinfo.js
+++ src/sysinfo.js
@@ -1,29 +1,42 @@
-let si	= require('systeminformation');
-let cpu	= require('cpu-stat');
-let os	= require('os');
+let si		= require('systeminformation');
+let cpu		= require('cpu-stat');
+let os		= require('os');
 
 let sysinfo = {
-	os: function() {
-		return {
-			arch: os.arch(),
-			homedir: os.homedir(),
-			hostname: os.hostname(),
-			ip: os.networkInterfaces(),
-			loadAvg: os.loadavg(),
-			platform: os.platform(),
-			release: os.release(),
-			type: os.type(),
-			uptime: os.uptime()
-		};
-	},
-	cpu: function() {
+	os: () => {
 		return new Promise((resolve, reject) => {
-			cpu.usagePercent(function(err, percent, seconds) {
-				if (err) {
-					reject(err);
-				} else {
-					resolve(percent);
-				}
+			si.getStaticData().then((res) => {
+				resolve(res);
+			}, (err) => {
+				reject(err);
+			});
+		});
+	},
+	cpu: () => {
+		return new Promise((resolve, reject) => {
+			si.currentLoad().then((res) => {
+				resolve(res);
+			}, (err) => {
+				reject(err);
+			});
+		});
+	},
+	mem: () => {
+		return new Promise((resolve, reject) => {
+			resolve({
+				freeBytes: os.freemem(),
+				totalBytes: os.totalmem(),
+				usedBytes: os.totalmem() - os.freemem(),
+				use: ((os.totalmem() - os.freemem())/os.totalmem() * 100).toFixed(2)
+			});
+		});
+	},
+	fs: () => {
+		return new Promise((resolve, reject) => {
+			si.fsSize((data) => {
+				resolve(data);
+			}, (err) => {
+				reject(err);
 			});
 		});
 	}
