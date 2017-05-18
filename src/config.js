8fa8670a7ee5eba136a003830fadd09fa8a5385a
nnkim <nnkim@scloudlab.com>
2017-05-18 15:04:43 +0900

    package.json) build: “webpack -p” >> “web pack” 변경 config.js) AETHER_URL : 개발기 집어넣음.

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const config = {
 	APP_NAME:	'AETHER MONITOR',
 	ROW:		'=================================================',
-	AETHER_URL:	'ws://localhost:8080/ws/agent',
+	AETHER_URL:	'ws://211.115.206.137:8080/ws/agent',
 	INTERVAL:	1000
 };
 
